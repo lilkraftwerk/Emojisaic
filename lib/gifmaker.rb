a8c9aa4afad313a8f3b8adbc6191703b04f34a50
Lucas Mathis <lucasquinnmathis@gmail.com>
2015-12-28 16:01:28 +0100

    small changes

--- lib/gifmaker.rb
+++ lib/gifmaker.rb
@@ -26,17 +26,17 @@ class GifMaker
     @new_filenames.length
   end
 
-  # commented out for now
-  # def make_preview(name, frames, start_at = 0)
-  #   old_gif = Magick::Image.read("output/#{name}.gif")
-  #   new_gif = Magick::ImageList.new
-  #   (start_at..start_at + frames).to_a.each do |frame_number|
-  #     new_gif << old_gif[frame_number]
-  #   end
-  #   output_dest = "output/#{name}-preview.gif"
-  #   new_gif.write(output_dest)
-  #   puts "wrote preview to #{output_dest}" unless @options[:quiet]
-  # end
+  ## no command line options for this yet
+  def make_preview(name, frames, start_at = 0)
+    old_gif = Magick::Image.read("output/#{name}.gif")
+    new_gif = Magick::ImageList.new
+    (start_at..start_at + frames).to_a.each do |frame_number|
+      new_gif << old_gif[frame_number]
+    end
+    output_dest = "output/#{name}-preview.gif"
+    new_gif.write(output_dest)
+    puts "wrote preview to #{output_dest}" unless @options[:quiet]
+  end
 
   def write_gif
     gif = Magick::ImageList.new
